4d61e6f8801249dbf451a2c4ae6009e5793e8cb3
Bao Lan <89245290+BaoLanNguyen@users.noreply.github.com>
2023-04-11 13:16:12 +0200

    a

--- Assets/scripts/cameraMovement.cs
+++ Assets/scripts/cameraMovement.cs
@@ -5,19 +5,5 @@ using UnityEngine;
 public class cameraMovement : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] private Camera cam;
-    private Vector3 dragOrigin;
-    private void update(){
-        PanCamera();
-    }
-    private void PanCamera()
-    {
-        if (Input.GetMouseButtonDown(0)){
-            dragOrigin = cam.ScreenToWorldPoint(Input.mousePosition);
-        }
-        if (Input.GetMouseButton(0))
-        {
-            Vector3 difference = dragOrigin - cam.ScreenToWorldPoint(Input.mousePosition);
-        }
-    }
+    private Vector3 Origin
 }
